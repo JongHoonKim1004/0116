906ac409455133de0908375edf38ea8f288dd7a0
JongHoonKim1004 <155927559+JongHoonKim1004@users.noreply.github.com>
2024-01-16 17:49:59 +0900

    Update header_ad.js

--- js/header_ad.js
+++ js/header_ad.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
     $("#header-ad-close-btn").click(function(){
         $("#header-ad").slideUp();
-        $("#fast-search-div").animate({top:"192px"});
-        $(".slider-box").animate({top:"206px"});
+        $("#fast-search-div").animate({top:"0px"});
+        $(".slider-box").animate({top:"0px"});
     });
 });
 
